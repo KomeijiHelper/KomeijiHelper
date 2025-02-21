8e27d806bab1147645685a03ec3c2fe822ef995d
KirisameVanilla <vanillaaaajp@gmail.com>
2025-02-21 13:33:06 +0800

    wip: HomeView

--- frontend/komeiji/src/router/index.js
+++ frontend/komeiji/src/router/index.js
@@ -2,11 +2,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginView from "@/views/LoginView.vue";
 import AboutView from "@/views/AboutView.vue";
+import HomeView from "@/views/HomeView.vue";
 
 const routes = [
     {
         path: "/",
         name: "Home",
+        component: HomeView,
+    },
+    {
+        path: "/login",
+        name: "Login",
         component: LoginView,
     },
     {
